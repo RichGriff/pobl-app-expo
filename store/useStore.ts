ac3bf62bec0edb0e7155db4525e21e7a33e98a56
Richard Griffiths <rich.griffiths89@gmail.com>
2025-05-01 16:26:20 +0100

    Logged in persist

--- store/useStore.ts
+++ store/useStore.ts
@@ -1,13 +1,37 @@
+// import { create } from 'zustand';
+
+// interface StoreState {
+//   isLoggedIn: boolean;
+//   setIsLoggedIn: (value: boolean) => void;
+// }
+
+// const useStore = create<StoreState>((set) => ({
+//   isLoggedIn: false,
+//   setIsLoggedIn: (value) => set({ isLoggedIn: value }),
+// }));
+
+// export default useStore;
+
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface StoreState {
   isLoggedIn: boolean;
   setIsLoggedIn: (value: boolean) => void;
 }
 
-const useStore = create<StoreState>((set) => ({
-  isLoggedIn: false,
-  setIsLoggedIn: (value) => set({ isLoggedIn: value }),
-}));
+const useStore = create<StoreState>()(
+  persist(
+    (set) => ({
+      isLoggedIn: false,
+      setIsLoggedIn: (value) => set({ isLoggedIn: value }),
+    }),
+    {
+      name: 'auth-storage',
+      storage: createJSONStorage(() => AsyncStorage)
+    }
+  )
+);
 
 export default useStore;
